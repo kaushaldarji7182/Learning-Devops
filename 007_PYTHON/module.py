ae8523fcfbe2a2e58d665cc2636740e3e94c8676
kaushaldarji7182 <kaushaldarji7182@gmail.com>
2025-02-09 11:23:18 +0530

    Added module.py and module1.py file

---
+++ 007_PYTHON/module.py
@@ -0,0 +1,18 @@
+def f(fname, lname):
+    return fname + " " + lname
+
+def sumoftwono(a, b):
+    return a + b
+
+person = {
+    "fname": "Kaushal",
+    "lname": "Darji",  
+    "age": 21,
+    "Aid": 123456,
+    "friends": ["skc", "ak"],
+    "skills": {
+        "backed_skills": ["nodejs", "ruby"],
+        "database": ["redis", "mysql", "postgress"]
+    }
+}
+
---
+++ 007_PYTHON/module1.py
@@ -0,0 +1,5 @@
+from module import sumoftwono as sum, f as fun, person as p
+
+print(sum(2, 5))  
+print(fun("Kaushal", "Darji")) 
+print(p)  
